cf61dd89706030586fbc108e15125900dd831f2a
Michal Pecio <michal.pecio@students.mimuw.edu.pl>
2011-07-07 13:24:07 +0200

    TagIndex

---
+++ TagIndex.cpp
@@ -0,0 +1,24 @@
+#include "TagIndex.hpp"
+using namespace std;
+using namespace boost;
+
+void DB::insert(MLCitation& cit) {
+	journal.insert(make_pair(cit.journal, cit.pmid));
+	date.insert(make_pair(cit.date, cit.pmid));
+}
+
+static inline vector<pmid_t> do_query(mimap &m, string s) {
+	vector<pmid_t> ret;
+	pair<mimap::iterator, mimap::iterator> range = m.equal_range(s);
+	for (mimap::iterator i = range.first; i != range.second; ++i)
+		ret.push_back(i->second);
+	return ret;
+}
+
+vector<pmid_t> DB::query_journal(string s) {
+	return do_query(journal, s);
+}
+
+vector<pmid_t> DB::query_date(string s) {
+	return do_query(date, s);
+}
---
+++ TagIndex.hpp
@@ -0,0 +1,17 @@
+#include <utility>
+#include <vector>
+#include <boost/unordered_map.hpp>
+#include "MLCitation.hpp"
+
+typedef unordered_multimap<std::string, pmid_t> mimap;
+
+class TagIndex {
+	mimap	journal;
+	mimap	date;
+public:
+	std::vector<pmid_t> query_journal(std::string);
+	std::vector<pmid_t> query_date(std::string);
+
+	void insert(MLCitation&);
+};
+
