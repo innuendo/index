148d1fee065256ecdaf21980308f489b0450c9e3
Michal Pecio <michal.pecio@students.mimuw.edu.pl>
2011-07-08 14:55:51 +0200

    XMLStore

---
+++ XMLStore.cpp
@@ -0,0 +1,70 @@
+#include "rapidxml_print.hpp"
+#include "XMLStore.hpp"
+#include <iostream>
+using namespace std;
+using namespace boost;
+using namespace rapidxml;
+
+#define MLCS_PER_FILE 30000
+
+XMLStore::XMLStore(char const* fileName) {
+	FILE *fp;
+	long len;
+	fp = fopen(fileName ,"r");
+	assert(fp != NULL);
+	fseek(fp, 0, SEEK_END);
+	len = ftell(fp);
+	fseek(fp, 0, SEEK_SET); 
+	read_data = (char*)malloc(len);
+	fread(read_data, len, 1, fp);
+	fclose(fp);
+	parse();
+}
+
+XMLStore::~XMLStore() {
+	free(read_data);
+}
+/*
+char* XMLStore::getAbstract(rapidxml::xml_node<> *node) {
+    if (node->first_node("Article")->first_node("Abstract")) {
+            return node->first_node("Article")->first_node("Abstract")->value();
+    } else if (node->first_node("Article")->first_node("OtherAbstract")) {
+        return node->first_node("Article")->first_node(
+            "OtherAbstract")->first_node("AbstractText")->value();
+    } else {
+        return NULL;
+    }
+}
+
+char* XMLStore::getTitle(rapidxml::xml_node<> *node) {
+    if (node->first_node("Article")->first_node("ArticleTitle")) {
+        return node->first_node("Article")->first_node("ArticleTitle")->value();
+    } else {
+        return NULL;
+    }
+}
+*/
+const char* XMLStore::get_xml(pmid_t id) {
+	unordered_map<pmid_t, string>::const_iterator i = xmls.find(id);
+	if (i != xmls.end())
+		return i->second.c_str();
+	else
+		return NULL;
+}
+
+void XMLStore::parse() {
+	rapidxml::xml_document<> doc;
+	doc.parse<0>(read_data);
+	rapidxml::xml_node<> *node = doc.first_node();	// <MedlineCitationSet>
+	node = node->first_node();			// <MedlineCitation>
+	while (node != NULL) {
+		if (node->first_node("PMID")) {
+			pmid_t pmid = boost::lexical_cast<uint64_t>(
+					node->first_node("PMID")->value());
+			string xml;
+			print(std::back_inserter(xml), *node, 0);
+			xmls[pmid] = xml;
+		}
+		node = node->next_sibling();
+	}
+}
